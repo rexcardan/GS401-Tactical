04d6d65da25a688e54864c933d5b54b118fb2429
Rex Cardan <rexcardan@gmail.com>
2024-03-09 07:25:50 -0600

    Exervise 9

---
+++ ContourCheckerUI/ChecklistBuilder.cs
@@ -0,0 +1,22 @@
+﻿using ContourChecker.API.Models;
+using ContourChecker.API.Models.Checks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContourChecker
+{
+    public class ChecklistBuilder
+    {
+        public static StructureChecklist BuildChecklist()
+        {
+            var checklist = new StructureChecklist();
+            checklist.AllChecks.Add(new Tg263Check());
+            checklist.AllChecks.Add(new AllStructuresInsideBodyCheck());
+            checklist.AllChecks.Add(new TargetWrappingCheck());
+            return checklist;
+        }
+    }
+}
---
+++ ContourCheckerUI/StructureChecklist.cs
@@ -0,0 +1,36 @@
+﻿using ContourChecker.API.Models;
+using Spectre.Console;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+
+namespace ContourChecker
+{
+    public class StructureChecklist
+    {
+        public List<StructureCheck> AllChecks { get; set; } = new List<StructureCheck>();
+        public List<CheckResult> EvaluateSet(StructureSet ss, ProgressContext ctx)
+        {
+            var results = new List<CheckResult>();
+            foreach (var s in ss.Structures)
+            {
+                var strTask = ctx.AddTask($"[green]Analyzing {s.Id}...[/]");
+                var checkCount = AllChecks.Count(c => c.IsEnabled);
+
+                for (int i = 0; i < AllChecks.Count; i++)
+                {
+                    var c = AllChecks[i];
+                    strTask.Increment(i*100.0/checkCount);
+                    if (c.IsEnabled)
+                    {
+                        results.Add(c.IsPassing(s, ss));
+                    }
+                }
+            }
+            return results;
+        }
+    }
+}
