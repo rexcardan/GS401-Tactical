04f89eb2b231ff3e85db9c7c99ad2d34a103a01a
Rex Cardan <rexcardan@gmail.com>
2024-03-09 05:36:20 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContourChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
