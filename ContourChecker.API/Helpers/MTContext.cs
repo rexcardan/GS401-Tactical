3fcc2ffe8ea442af90a0b4f20d2072ac64fe057f
Rex Cardan <rexcardan@gmail.com>
2024-03-09 11:16:31 -0600

    added multithreading helpers

---
+++ ContourChecker.API/Helpers/MTContext.cs
@@ -0,0 +1,292 @@
+﻿using ESAPIX.Common.Args;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using VMS.TPS.Common.Model.API;
+
+namespace ContourChecker.API.Helpers
+{
+    /// <summary>
+    ///     Wraps a VMS application in a way that replicates the ScriptContext class. Can be used for debugging or building two
+    ///     sided apps
+    /// </summary>
+    public sealed class MTContext : IDisposable
+    {
+        public MTContext(Application app)
+        {
+            Exception e = null;
+
+            Application = app;
+
+            if (Application == null)
+                throw new Exception("App was not created. Check to make sure the VMS dll references are correct.", e);
+
+            CurrentUser = Application?.CurrentUser;
+            Logger = LogManager.GetCurrentClassLogger();
+        }
+
+        public PlanningItem GetPlanFromID(string planID, string courseID, bool isPlanSum)
+        {
+            if (isPlanSum)
+            {
+                return Patient.Courses.FirstOrDefault(c => c.Id == courseID)?
+                    .PlanSums.FirstOrDefault(p => p.Id == planID);
+            }
+            else
+            {
+                return Patient.Courses.FirstOrDefault(c => c.Id == courseID)?
+                    .PlanSetups.FirstOrDefault(p => p.Id == planID);
+            }
+        }
+
+        public void Dispose()
+        {
+            Application?.Dispose();
+        }
+
+        public Application Application { get; }
+
+        public Course Course { get; set; }
+
+        public User CurrentUser { get; set; }
+
+        public Image Image { get; set; }
+
+        public Patient Patient { get; set; }
+
+        public PlanSetup PlanSetup { get; set; }
+
+        public PlanSum PlanSum { get; set; }
+
+        public IEnumerable<PlanSetup> PlansInScope { get; set; } = new List<PlanSetup>();
+
+        public IEnumerable<PlanSum> PlanSumsInScope { get; set; } = new List<PlanSum>();
+
+        public StructureSet StructureSet { get; set; }
+
+        public Multithread Thread { get; set; }
+
+        public Dispatcher UIDispatcher { get; set; }
+
+#if !VMS110
+        public string ApplicationName { get; set; } = "VMS Application";
+
+        public BrachyPlanSetup BrachyPlanSetup { get; private set; }
+
+        public IEnumerable<BrachyPlanSetup> BrachyPlansInScope { get; set; }
+
+        public ExternalPlanSetup ExternalPlanSetup { get; private set; }
+
+        public IEnumerable<ExternalPlanSetup> ExternalPlansInScope { get; set; }
+#endif
+        public ILogger Logger { get; private set; }
+
+        public bool SetPatient(string id)
+        {
+            Application.ClosePatient();
+            Patient = Application.OpenPatientById(id);
+            var found = Patient != null;
+            if (found)
+                OnPatientChanged(Patient);
+            else
+                OnPatientChanged(null);
+            return found;
+        }
+
+        public bool SetCourse(Course course, bool autoFillChildren = true)
+        {
+            Course = course;
+            if (autoFillChildren)
+            {
+                PlanSumsInScope = course?.PlanSums;
+                PlansInScope = course?.PlanSetups;
+            }
+            //Notify
+            OnCourseChanged(course);
+            return Course != null;
+        }
+
+        public bool SetPlanSetup(PlanSetup plan, bool autoFillChildren = true)
+        {
+            PlanSetup = plan;
+            if (autoFillChildren)
+            {
+                StructureSet = plan?.StructureSet;
+                Image = plan?.StructureSet?.Image;
+            }
+            //Notify
+            OnPlanSetupChanged(plan);
+            return PlanSetup != null;
+        }
+
+        public void SetPlanSumsInScope(IEnumerable<PlanSum> sums)
+        {
+            PlanSumsInScope = sums;
+        }
+
+        public void SetImage(Image im)
+        {
+            Image = im;
+        }
+
+        public void SetPlanSum(PlanSum plan)
+        {
+            PlanSum = plan;
+        }
+
+        public void ClosePatient()
+        {
+            Application.ClosePatient();
+            this.Patient = null;
+            this.PlanSetup = null;
+            this.Course = null;
+#if !VMS110
+            this.BrachyPlanSetup = null;
+            this.ExternalPlanSetup = null;
+            SetBrachyPlanSetup(null);
+            SetExternalPlanSetup(null);
+#endif
+#if (VMS150 || VMS151 || VMS155)
+            SetIonPlanSetup(null);
+#endif
+            OnPatientChanged(null);
+            SetPlanSetup(null);
+            SetCourse(null);
+        }
+
+        public void SetPlansInScope(IEnumerable<PlanSetup> plans)
+        {
+            PlansInScope = plans;
+        }
+
+#if !VMS110
+        public void SetExternalPlansInScope(IEnumerable<ExternalPlanSetup> plans)
+        {
+            ExternalPlansInScope = plans;
+        }
+
+        public void SetBrachyPlansInScope(IEnumerable<BrachyPlanSetup> plans)
+        {
+            BrachyPlansInScope = plans;
+        }
+
+        public bool SetExternalPlanSetup(ExternalPlanSetup ex)
+        {
+            PlanSetup = ex;
+            ExternalPlanSetup = ex;
+            //Notify
+            OnExternalPlanSetupChanged(ex);
+            OnPlanSetupChanged(ex);
+            return ExternalPlanSetup != null;
+        }
+
+        public bool SetBrachyPlanSetup(BrachyPlanSetup bs)
+        {
+            PlanSetup = bs;
+            BrachyPlanSetup = bs;
+            //Notify
+            OnBrachyPlanSetupChanged(bs);
+            OnPlanSetupChanged(bs);
+            return BrachyPlanSetup != null;
+        }
+#endif
+
+        #region CONTEXT CHANGED EVENTS
+
+        public delegate void PatientChangedHandler(Patient newPatient);
+
+#if (VMS150 || VMS151 || VMS155)
+        public IonPlanSetup IonPlanSetup { get; set; }
+        public IEnumerable<IonPlanSetup> IonPlansInScope { get; set; }
+
+        public void SetIonPlansInScope(IEnumerable<IonPlanSetup> plans)
+        {
+            IonPlansInScope = plans;
+        }
+
+        public bool SetIonPlanSetup(IonPlanSetup plan, bool autoFillChildren = true)
+        {
+            IonPlanSetup = plan;
+            if (autoFillChildren)
+            {
+                StructureSet = plan?.StructureSet;
+                Image = plan?.StructureSet?.Image;
+            }
+            //Notify
+            OnIonPlanSetupChanged(plan);
+            return IonPlanSetup != null;
+        }
+
+        public delegate void IonPlanSetupChangedHandler(IonPlanSetup ps);
+        public event StandAloneContext.IonPlanSetupChangedHandler IonPlanSetupChanged;
+
+        public void OnIonPlanSetupChanged(IonPlanSetup ps)
+        {
+            IonPlanSetupChanged?.Invoke(ps);
+        }
+#endif
+
+        public event PatientChangedHandler PatientChanged;
+
+        public void OnPatientChanged(Patient p)
+        {
+            PatientChanged?.Invoke(p);
+        }
+
+        public delegate void PlanSetupChangedHandler(PlanSetup ps);
+
+        public event PlanSetupChangedHandler PlanSetupChanged;
+
+        public void OnPlanSetupChanged(PlanSetup ps)
+        {
+            PlanSetupChanged?.Invoke(ps);
+        }
+
+#if !VMS110
+        public delegate void ExternalPlanSetupChangedHandler(ExternalPlanSetup ps);
+
+        public event ExternalPlanSetupChangedHandler ExternalPlanSetupChanged;
+
+        public void OnExternalPlanSetupChanged(ExternalPlanSetup ps)
+        {
+            ExternalPlanSetupChanged?.Invoke(ps);
+        }
+
+        public delegate void BrachyPlanSetupChangedHandler(BrachyPlanSetup ps);
+
+        public event BrachyPlanSetupChangedHandler BrachyPlanSetupChanged;
+
+        public void OnBrachyPlanSetupChanged(BrachyPlanSetup ps)
+        {
+            BrachyPlanSetupChanged?.Invoke(ps);
+        }
+#endif
+        public delegate void CourseChangedHandler(Course c);
+
+        public event CourseChangedHandler CourseChanged;
+
+        public void OnCourseChanged(Course c)
+        {
+            CourseChanged?.Invoke(c);
+        }
+
+        public async Task<T> GetValueAsync<T>(Func<MTContext, T> toExecute)
+        {
+            var result = default(T);
+            await Thread.InvokeAsync(() => { result = toExecute(this); });
+            return result;
+        }
+
+        public T GetValue<T>(Func<MTContext, T> toExecute)
+        {
+            var result = default(T);
+            Thread.Invoke(() => { result = toExecute(this); });
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ ContourChecker.API/Helpers/Multithread.cs
@@ -0,0 +1,245 @@
+﻿using ESAPIX.Common;
+using ESAPIX.Logging;
+using NLog;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+
+namespace ContourChecker.API.Helpers
+{
+    public class Multithread : IDisposable
+    {
+        private BlockingCollection<Task> _vmsJobs = new BlockingCollection<Task>();
+        private BlockingCollection<Task> _nonVMSJobs = new BlockingCollection<Task>();
+        private static Multithread instance = null;
+        private static readonly object padlock = new object();
+        private Thread vmsThread;
+        private Thread nonVMSThread;
+        private MTContext _sac;
+        private CancellationTokenSource cts;
+
+        private static NLog.ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        private Multithread()
+        {
+            cts = new CancellationTokenSource();
+            nonVMSThread = new Thread(NonVMSThreadStart)
+            {
+                Name = "ACT NonVMS",
+                IsBackground = true
+            };
+            nonVMSThread.SetApartmentState(ApartmentState.STA);
+            nonVMSThread.Start();
+
+            vmsThread = new Thread(VMSThreadStart)
+            {
+                Name = "ACT VMS",
+                IsBackground = true
+            };
+            vmsThread.SetApartmentState(ApartmentState.STA);
+            vmsThread.Start();
+        }
+
+        private void NonVMSThreadStart()
+        {
+            foreach (var job in _nonVMSJobs.GetConsumingEnumerable(cts.Token))
+            {
+                try
+                {
+                    job.RunSynchronously();
+                }
+                catch (Exception ex)
+                {
+                    _logger.Error(ex);
+                    OnExceptionRaisedHandler(ex);
+                }
+            }
+        }
+
+        private void VMSThreadStart()
+        {
+            foreach (var job in _vmsJobs.GetConsumingEnumerable(cts.Token))
+            {
+                try
+                {
+                    job.RunSynchronously();
+                }
+                catch (Exception ex)
+                {
+                    NonVMSInvoke(() =>
+                    {
+                        _logger.Error(ex);
+                        OnExceptionRaisedHandler(ex);
+                    });
+                }
+            }
+        }
+
+        public static Multithread Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Multithread();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public Task SetContext(Func<VMS.TPS.Common.Model.API.Application> createAppFunc)
+        {
+            return InvokeAsync(new Action(() =>
+            {
+                Application app = null;
+                try
+                {
+                    app = createAppFunc();
+                }
+                catch (Exception e)
+                {
+                    throw e;
+                }
+                _sac = new MTContext(app);
+                _sac.Thread = this;
+
+                NonVMSInvoke(() =>
+                {
+                    RaiseContextLoaded();
+                });
+            }));
+        }
+
+        public T GetValue<T>(Func<MTContext, T> sacFunc)
+        {
+            T toReturn = default(T);
+            Invoke(() =>
+            {
+                toReturn = sacFunc(_sac);
+            });
+            return toReturn;
+        }
+
+        public async Task<T> GetValueAsync<T>(Func<MTContext, T> sacFunc)
+        {
+            T toReturn = default(T);
+            await InvokeAsync(() =>
+            {
+                toReturn = sacFunc(_sac);
+            });
+            return toReturn;
+        }
+
+        public void Execute(Action<MTContext> sacOp)
+        {
+            Invoke(() =>
+            {
+                sacOp(_sac);
+            });
+        }
+
+        public Task ExecuteAsync(Action<MTContext> sacOp)
+        {
+            return InvokeAsync(() =>
+            {
+                sacOp(_sac);
+            });
+        }
+
+        public Task InvokeAsync(Action action)
+        {
+            var task = new Task(action, cts.Token);
+            _vmsJobs.Add(task);
+            return task;
+        }
+
+        public void Invoke(Action action)
+        {
+            var task = new Task(action, cts.Token);
+            _vmsJobs.Add(task);
+            try
+            {
+                task.GetAwaiter().GetResult();
+                if (task.IsFaulted)
+                {
+                    throw task.Exception;
+                }
+            }
+            catch (Exception ex)
+            {
+                NonVMSInvoke(() =>
+                {
+                    _logger.Error(ex);
+                    OnExceptionRaisedHandler(ex);
+                });
+                throw;
+            }
+        }
+
+        private Task NonVMSInvoke(Action action)
+        {
+            var task = new Task(action);
+            _nonVMSJobs.Add(task);
+            return task;
+        }
+
+        public void DisposeVMS()
+        {
+            Invoke(new Action(() =>
+            {
+                if (_sac != null)
+                {
+                    _sac.Application?.Dispose();
+                    _sac = null;
+                }
+            }));
+        }
+
+        public void Dispose()
+        {
+            DisposeVMS();
+            if (!_vmsJobs.IsAddingCompleted)
+            {
+                while (_vmsJobs.Count > 0)
+                {
+                    Task item;
+                    _vmsJobs.TryTake(out item);
+                }
+                _vmsJobs.CompleteAdding();
+                _nonVMSJobs.CompleteAdding();
+            }
+        }
+
+        public int ThreadId => vmsThread.ManagedThreadId;
+
+        public delegate void ExceptionRaisedHandler(Exception ex);
+        public event ExceptionRaisedHandler ExceptionRaised;
+
+        public void OnExceptionRaisedHandler(Exception ex)
+        {
+            ExceptionRaised?.Invoke(ex);
+        }
+
+        public event EventHandler ContextLoaded;
+
+        public void RaiseContextLoaded()
+        {
+            ContextLoaded?.Invoke(this, EventArgs.Empty);
+        }
+
+        public event EventHandler ThreadStopping;
+
+        public void RaiseThreadStopping()
+        {
+            ThreadStopping?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
