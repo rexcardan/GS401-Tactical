67f5c9a24554e454713d8cf4854e3e6ae7b18671
Rex Cardan <rexcardan@gmail.com>
2024-03-09 07:35:33 -0600

    Final console UI

--- ContourCheckerUI/Program.cs
+++ ContourCheckerUI/Program.cs
@@ -1,6 +1,10 @@
 ﻿using CommandLine;
+using ContourChecker.API.Models;
 using NLog;
+using Spectre.Console;
+using Spectre.Console.Rendering;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using VMS.TPS.Common.Model.API;
 
@@ -17,6 +21,9 @@ namespace ContourChecker
 
             //For testing purposes
             args = new string[] { "-p", "Prostate003", "-s", "2.25.137385552897349939998230737929399514051" };
+            
+            var checklist = ChecklistBuilder.BuildChecklist();
+            var results = new List<CheckResult>();
 
             Parser.Default.ParseArguments<EsapiState>(args)
             .WithParsed(state =>
@@ -38,9 +45,51 @@ namespace ContourChecker
                         return;
                     }
 
-                    Console.WriteLine("Patient: {0}, Structure Set: {1}", patient.Name, ss.Id);
+                    AnsiConsole.Progress()
+                         .Start(ctx =>
+                         {
+                             results = checklist.EvaluateSet(ss, ctx);
+                         });
                 }
             });
+
+            //Render results
+            var table = new Table();
+            // Add some columns
+            table.AddColumn("Passed").Centered();
+            table.AddColumn("Constraint");
+            table.AddColumn("Message");
+
+            foreach (var res in results)
+            {
+                if (res.IsPassed)
+                {
+                    var renderable = new List<IRenderable>
+                        {
+                            new Panel("[green]O[/]"),
+                            new Markup(res.CheckName),
+                            new Markup(res.Message)
+                        };
+
+                    table.AddRow(renderable);
+                }
+                else
+                {
+                    var renderable = new List<IRenderable>
+                       {
+                            new Panel("[red]X[/]"),
+                            new Markup(res.CheckName),
+                            new Markup(res.Message)
+                        };
+
+                    table.AddRow(renderable);
+                }
+
+            }
+
+
+            // Render the table to the console
+            AnsiConsole.Write(table);
 
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
