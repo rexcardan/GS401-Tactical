750748d03f517507e46202b79e399c2d87811834
Rex Cardan <rexcardan@gmail.com>
2024-03-09 06:16:45 -0600

    Exercise 4

---
+++ LogHelper.cs
@@ -0,0 +1,31 @@
+﻿using NLog;
+using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContourChecker
+{
+    public class LogHelper
+    {
+        public static void SetupLogging()
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+            // Targets where to log to: File and Console
+            var logfile = new FileTarget("logfile")
+            {
+                FileName = "${basedir}/AppData/Logs/log.txt",
+                Layout = "${longdate} ${level} ${message}  ${exception} ${event-properties:myProperty}"
+            };
+            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,20 +1,45 @@
 ﻿using CommandLine;
+using NLog;
 using System;
+using System.Linq;
+using VMS.TPS.Common.Model.API;
 
 namespace ContourChecker
 {
     internal class Program
     {
+        private static ILogger _logger = LogManager.GetCurrentClassLogger();
+
+        [STAThread]
         static void Main(string[] args)
         {
+            LogHelper.SetupLogging();
+
             //For testing purposes
             args = new string[] { "-p", "Prostate003", "-s", "2.25.137385552897349939998230737929399514051" };
 
             Parser.Default.ParseArguments<EsapiState>(args)
             .WithParsed(state =>
             {
-                Console.WriteLine($"PatientId: {state.PatientId}");
-                Console.WriteLine($"StructureSetUID: {state.StructureSetUID}");
+                using (var app = Application.CreateApplication())
+                {
+                    _logger.Info($"{app.CurrentUser.Id} operating on {state.PatientId}, structure set {state.StructureSetUID}");
+
+                    var patient = app.OpenPatientById(state.PatientId);
+                    if (patient == null)
+                    {
+                        Console.WriteLine("Patient not found");
+                        return;
+                    }
+                    var ss = patient.StructureSets.FirstOrDefault(s => s.UID == state.StructureSetUID);
+                    if (ss == null)
+                    {
+                        Console.WriteLine("Structure set not found");
+                        return;
+                    }
+
+                    Console.WriteLine("Patient: {0}, Structure Set: {1}", patient.Name, ss.Id);
+                }
             });
 
             Console.WriteLine("Press any key to exit...");
